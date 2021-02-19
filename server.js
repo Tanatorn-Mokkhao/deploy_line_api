9d362bbf5a502286a5cd7ec3394a86afb698362a
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 02:00:42 +0700

    update commit

--- server.js
+++ server.js
@@ -15,9 +15,37 @@ app.get("/", function (req, res) {
 });
 
 app.post("/webhook", (req, res) => {
+  let reply_token = req.body.events[0].replyToken;
+  reply(reply_token);
   res.sendStatus(200);
 });
 
-app.listen(app.get("port"), function () {
-  console.log("run at port", app.get("port"));
+app.listen(app.get("port"), function (reply_token) {
+  let headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer {xxxxxxx}",
+  };
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: "text",
+        text: "Hello",
+      },
+      {
+        type: "text",
+        text: "How are you?",
+      },
+    ],
+  });
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers: headers,
+      body: body,
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode);
+    }
+  );
 });
