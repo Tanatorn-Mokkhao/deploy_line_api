5a8616a3d68c8a4e2b1ae56055fb7b88e67cf581
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 02:53:02 +0700

    update commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", function (req, res) {
 
 app.post("/webhook", (req, res) => {
   let reply_token = req.body.events[0].replyToken;
-  reply(reply_token);
+  //   reply(reply_token);
   res.sendStatus(200);
 });
 
