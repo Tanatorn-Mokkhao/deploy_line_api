b4f444cf011b49517fa6f79eb099d2a835841df7
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 01:05:03 +0700

    first-commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set("port", process.env.PORT || 4000);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(app.get("port"), function () {
+  console.log("run at port", app.get("port"));
+});
