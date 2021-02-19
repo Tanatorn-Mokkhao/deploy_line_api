f1e18159e15521a6e9f9c96da4b7dfe9da29d268
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 02:47:26 +0700

    update commit

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const cors = require("cors");
+
 const app = express();
-const port = process.env.PORT || 4000;
+app.use(cors());
+app.set("port", process.env.PORT || 4000);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.post("/webhook", (req, res) => {
   //   let reply_token = req.body.events[0].replyToken;
   //   reply(reply_token);
   res.sendStatus(200);
 });
-app.listen(port);
+app.listen(app.get("port"), function () {
+  console.log("run at port", app.get("port"));
+});
 // function reply(reply_token) {
 //   let headers = {
 //     "Content-Type": "application/json",
