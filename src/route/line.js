6091e607b4100b12bec79b9c2c8fcdfdb709f48a
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 11:40:24 +0700

    update commit

--- src/route/line.js
+++ src/route/line.js
@@ -6,13 +6,50 @@ const line = require("@line/bot-sdk");
 const env = require("dotenv");
 env.config();
 
+// const config = {
+//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+//   channelSecret: process.env.CHANNEL_SECRET,
+// };
+
+// const client = new line.Client(config);
+
+// router.post("/callback", line.middleware(config), linebot);
+
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
+// create LINE SDK client
 const client = new line.Client(config);
 
-router.post("/callback", line.middleware(config), linebot);
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+router.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: "text", text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
 
 module.exports = router;
