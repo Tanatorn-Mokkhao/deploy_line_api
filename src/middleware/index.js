a1adaef07a1ff02c93e687e41ab76f5b346e8c90
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 11:45:27 +0700

    update commit

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,16 +1,11 @@
 const line = require("@line/bot-sdk");
 
 exports.lineMiddleware = (req, res, next) => {
-  try {
-    const config = {
-      channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-      channelSecret: process.env.CHANNEL_SECRET,
-    };
-    const client = new line.Client(config);
-    line.middleware(config);
-
-    next();
-  } catch (error) {
-    console.log("sad");
-  }
+  const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+  };
+  const client = new line.Client(config);
+  line.middleware(config);
+  next();
 };
