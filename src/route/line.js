ede781d208cf90c7c3c6319a8113d9aba52b28d4
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 11:09:04 +0700

    update commit

--- src/route/line.js
+++ src/route/line.js
@@ -3,6 +3,6 @@ const { line } = require("../controller/line");
 const { lineMiddleware } = require("../middleware");
 const router = express.Router();
 
-router.get("callback", lineMiddleware, line);
+router.get("/callback", lineMiddleware, line);
 
 module.exports = router;
