b00b56cbe4fe65e9351ca26140fc5e1f67de0f79
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-02-20 11:04:53 +0700

    update commit

---
+++ src/middleware/index.js
@@ -0,0 +1,13 @@
+const line = require("@line/bot-sdk");
+
+exports.lineMiddleware = (req, res, next) => {
+  try {
+    const config = {
+      channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+      channelSecret: process.env.CHANNEL_SECRET,
+    };
+    const client = new line.Client(config);
+    line.middleware(config);
+    next();
+  } catch (error) {}
+};
---
+++ src/route/line.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { line } = require("../controller/line");
+const { lineMiddleware } = require("../middleware");
+const router = express.Router();
+
+router.get("callback", lineMiddleware, line);
+
+module.exports = router;
